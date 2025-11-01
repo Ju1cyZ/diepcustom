99e2211bd39ea6f02d8d2febda7f29f94dcb4b5a
JuicyZ <fabiosj23@hotmail.com>
2025-11-01 21:05:27 +0100

    Create SpeedModeGlobals.ts

---
+++ src/Const/SpeedModeGlobals.ts
@@ -0,0 +1,14 @@
+
+// -------------------------------------------------
+// SQUARES, TRIANGLES, PENTAGONE, HEXAGONE MODIFIERS
+// -------------------------------------------------
+
+export const DividedHealth = 2; // Half the health
+
+export const MultipliedRewardAmount = 1.5; // 50% more reward
+export const MultipliedSquaresTrianglesPentagoneAmount = 2; // Twice the amount of squares, triangles pentagons and hexagones
+export const HigherTierProbabiltyMultiplierTrianglesPentagone = 2; // Twice the chance for higher tier shapes (triangles, pentagons and hexagones)
+
+export const ShinyProbability = 0.02; // 2% chance for shapes to be shiny
+export const ShinyRewardMulitplier = 50; // 50 times the reward for shiny shapes
+export const ShinyHealthMultiplier = 5; // 5 times the health for shiny shapes
